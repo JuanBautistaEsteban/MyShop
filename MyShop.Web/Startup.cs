b03c6ec1fe9a126e9d8615ba76df28a5b21e03e2
JuanBautistaEsteban <jbesteban.personal@gmail.com>
2022-12-01 11:31:04 +0100

    Creados los proyectos y añadidas las referencias de los proyectos.

---
+++ MyShop.Web/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyShop.Web.Startup))]
+namespace MyShop.Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
